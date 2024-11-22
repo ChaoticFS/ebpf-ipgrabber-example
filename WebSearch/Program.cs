7d2814f5e40a07b8bf25b0eba2ec0a22684f9c8d
ChaoticFS <104865518+ChaoticFS@users.noreply.github.com>
2024-11-22 04:26:44 +0100

    websearch updated for k8s

--- WebSearch/Program.cs
+++ WebSearch/Program.cs
@@ -3,10 +3,18 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebSearch;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+if (builder.HostEnvironment.Environment == "Production")
+{
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://searchapi-service:5262") });
+}
+else
+{
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5262") });
+}
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5262") });
 
 await builder.Build().RunAsync();
