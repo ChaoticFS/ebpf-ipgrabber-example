d123c46f4c9818a6ad3188352d14167f0f64a389
ChaoticFS <Magnus.overgaard1711@gmail.com>
2024-12-13 09:22:34 +0100

    mapped prometheus endpoints

--- SearchAPI/Program.cs
+++ SearchAPI/Program.cs
@@ -1,6 +1,7 @@
 using Shared;
 using SearchAPI.Database;
 using SearchAPI.Services;
+using OpenTelemetry.Metrics;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddPrometheusExporter();
+
+        builder.AddMeter("Microsoft.AspNetCore.Hosting",
+                         "Microsoft.AspNetCore.Server.Kestrel");
+        builder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
+                       0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+            });
+    });
 
 var redisConnectionString = builder.Configuration["Redis:ConnectionString"];
 
@@ -46,6 +62,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapPrometheusScrapingEndpoint();
+
+app.MapGet("/telemetry", () => "OpenTelemetry! ticks:"
+                     + DateTime.Now.Ticks.ToString()[^5..]);
 
 app.UseCors(); //Activating Cors
 
